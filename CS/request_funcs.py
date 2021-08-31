556f86d4d892270954d7e32560d789b52af9b675
tomzhang0719 <zhangsiyuan777@gmail.com>
2021-08-31 00:35:00 -0400

    functions progress

--- CS/request_funcs.py
+++ CS/request_funcs.py
@@ -1,20 +1,107 @@
 import requests
+import json
+from urllib.parse import quote_plus
+from time import sleep
+from os import environ
 
 
-def player_request()
+# TODO - how? google
+def email_admin():
+    pass
 
 
+def player_request(player_tag: str, endpoint: str) -> dict:
+    """
+    A function to send a /GET request to Clash Royale API with comprehensive error handling.
+    :param player_tag: A string (e.g., '#9YJUPU9LY');
+    :param endpoint: 'info' | 'battlelog';
+    :return: json.
+    """
+    url = 'https://api.clashroyale.com/v1/players/{}/{}'.format(
+        quote_plus(player_tag), endpoint)
+
+    token = environ.get('TOKEN')
+    headers = {'Authorization': 'Bearer ' + token}
+
+    # request
+    try:
+        response = requests.get(url, headers=headers)
+    except:
+        # TODO send email
+        return {'statusCode': 500,
+                'body': {'message': '/GET request failed; url = {}; Bearer token = {}.'.format(url, token)}}
+
+    status_code = response.status_code
+    res = response.json()
+
+    # handle error responses
+    if status_code == 200:
+        pass
+
+    elif status_code == 429:
+        # request throttled: amount of requests exceeded threshold defined for API token
+        # switch to another api key & notify admin
+        api_keys = json.loads(environ.get('API_KEYS'))
+        curr_key = environ.get('CURR_KEY')
+        key_names = list(api_keys.keys())
+        curr_key_idx = key_names.index(curr_key)
+
+        if curr_key_idx >= len(api_keys) - 1:
+            # TODO email: use up all keys, require immediate action
+            print('used up all keys')
+            exit(1)
+        else:
+            # use next available key
+            next_key = key_names[curr_key_idx + 1]
+            token = str(api_keys.get(next_key))
+
+            # update environment variables
+            environ['CURR_KEY'] = next_key
+            environ['TOKEN'] = token
+
+            # TODO email admin
+
+            # retry request with new token
+            player_request(player_tag, endpoint)
+
+    elif status_code == 503:
+        # service temprorarily unavailable due to maintenance
+        i = 0
+        while True:
+            if i % 288 == 0:  # 1 iteration = 5 mins; 288 iterations = 1 day
+                # TODO email admin every day
+                print('service down for {} days'.format(i // 288))
+            i += 1
+            sleep(300)  # wait 5 mins then try again; loop
+            player_request(player_tag, endpoint)
+
+    else:
+        # other errors: simply email admin
+        pass
+        # TODO email admin
+
+    return {'statusCode': status_code, 'body': res}
 
 
+if __name__ == '__main__':
+    # ==========================================
+    # load api key
+    try:
+        f = open('api_keys.json')
+    except FileNotFoundError:
+        print('api_keys.json not found. Make sure SYE/CS/ is your pwd.')
+        exit(1)
+        # TODO send email and quit instead?
 
+    api_keys = json.load(f)
+    curr_key = "Tom's Key"
+    token = str(api_keys.get(curr_key))
 
-url = 'https://api.clashroyale.com/v1/players/%239YJUPU9LY/battlelog'
+    # save vars as environment variable
+    environ['API_KEYS'] = json.dumps(api_keys)
+    environ['CURR_KEY'] = curr_key
+    environ['TOKEN'] = token
+    # ==========================================
 
-payload = {}
-headers = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQ0OTkxN2MyLWQ3ODgtNDZiNi1iMjZhLTY5NzM2ODNjNWJmZCIsImlhdCI6MTYzMDM1ODI5OSwic3ViIjoiZGV2ZWxvcGVyL2RjMWVmZjViLTg4MWEtNWVhMS0xNWQ3LTNmMzBmYTMxYzg1NiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI2OS42LjEwNy42NyIsIjY5LjYuMTA4LjE2OSIsIjY5LjYuMTExLjI5IiwiNjkuNi4xMDkuMTYxIiwiNjkuNi4xMTQuMTQ5Il0sInR5cGUiOiJjbGllbnQifV19.FO3jX8a0nVTDSlE_23uzpTp5xbxI_yvqv3SR7N6XPWao8Nt7-zgvtfoSsq1UDAKzVBSAm0qVkkoGTVjipwurww'
-}
-
-response = requests.request('GET', url, headers=headers, data=payload)
-
-print(response.text)
+    # testing
+    print(player_request('#9YJUPU9LY', 'battlelog'))
