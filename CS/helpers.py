bf7599e70228b5bc249318fb5e224c5cdf8ce39e
tomzhang0719 <zhangsiyuan777@gmail.com>
2021-11-03 15:11:29 -0400

    email admin except

--- CS/helpers.py
+++ CS/helpers.py
@@ -11,24 +11,27 @@ from os.path import dirname
 # A collection of globally-shared helper functions
 
 def email_admin(status_code, message):
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = "tom.zhang.dev@gmail.com"
-    receiver_email = "tom.zhang.dev@gmail.com"
-    pass_file = open(
-        '{}/credentials/dev-gmail-pass.txt'.format(dirname(__file__)))
-    password = pass_file.readline()
-    pass_file.close()
+    try:
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = "tom.zhang.dev@gmail.com"
+        receiver_email = "tom.zhang.dev@gmail.com"
+        pass_file = open(
+            '{}/credentials/dev-gmail-pass.txt'.format(dirname(__file__)))
+        password = pass_file.readline()
+        pass_file.close()
 
-    msg_template = """Subject: {}
+        msg_template = """Subject: {}
 
-    {}"""
+        {}"""
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email,
-                        msg_template.format(status_code, message))
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email,
+                            msg_template.format(status_code, message))
+    except:
+        print("email_admin() failed, printing in console instead:", status_code, message)
 
 
 def cr_api_request(tag: str, action: str, i: int = 0) -> dict:
