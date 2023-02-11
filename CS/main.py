8252c113145ddca3b508578fd925c36aa2ca87f7
choongsoo <choongsoo.lee@gmail.com>
2023-02-11 10:54:43 -0500

    main.py: log the process id

--- CS/main.py
+++ CS/main.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python3
+
 from helpers import init_log
 from data_collection import collect_data
 
+import os
+
+pidFile = open("main.pid", "w")
+pidFile.write(str(os.getpid()))
+pidFile.close()
 
 if __name__ == '__main__':
     init_log()
